d894751fd6ad51b01a3aedc12842e5f33f8803fd
Yurii Hladynets <yurii_hladynets@epam.com>
2019-09-11 12:55:03 +0300

    set up enviroment

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    webserver = require("gulp-webserver"),
+
+    source = './sass/',
+    dest = './builds/';
+
+
+
+gulp.task('html', function(){
+   return gulp.src(dest + '*.html');
+});
+
+gulp.task('sass', function(){
+   return gulp.src(source + 'style.scss')
+        .pipe(sass().on('erro', sass.logError))
+        .pipe(gulp.dest(dest  + 'css/'));
+});
+
+
+gulp.task('watch', function(){
+    return gulp.watch('./sass/style.scss', gulp.series('sass'));
+});
+
+
+gulp.task('webserver', function(){
+        return gulp.src(dest)
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', gulp.series('html', 'sass', 'webserver', 'watch'));
